dea06b811dd1b635e51f1e25ffb17763c193e545
Kilian <kilianc0709@gmail.com>
2023-09-16 22:00:51 +0200

    fix: enabled CORS for all origins

--- commodity_project/settings.py
+++ commodity_project/settings.py
@@ -57,10 +57,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
-    "http://localhost",
-]
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = "commodity_project.urls"
 
