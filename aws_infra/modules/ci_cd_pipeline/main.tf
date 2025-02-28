8644965921ec491b16835f3019ea0fd5084055b3
MaxE99 <me-99@live.de>
2025-02-28 18:35:41 +0100

    fix: github action condition

--- aws_infra/modules/ci_cd_pipeline/main.tf
+++ aws_infra/modules/ci_cd_pipeline/main.tf
@@ -21,8 +21,7 @@ resource "aws_iam_role" "github_actions" {
         Condition = {
           StringLike = {
             "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
-            "token.actions.githubusercontent.com:sub" = var.github_repository
-            "token.actions.githubusercontent.com:ref" = "refs/heads/main"
+            "token.actions.githubusercontent.com:sub" = "${var.github_repository}ref:refs/heads/main"
           }
         }
       }
